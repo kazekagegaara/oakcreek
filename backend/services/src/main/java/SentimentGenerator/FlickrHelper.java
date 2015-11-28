696947af1b6cae25b7848dd7340250c8c3e81de3
kazekagegaara <mansinkal2910@yahoo.co.in>
2015-11-27 20:04:26 -0700

    adding youtube and flickr helpers

---
+++ backend/services/src/main/java/SentimentGenerator/FlickrHelper.java
@@ -0,0 +1,51 @@
+package SentimentGenerator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+import java.util.*;
+
+
+public class FlickrHelper {
+ private static key = "xxxxxxxxxx"; // put your flickr key here
+
+ public List<FlickrObject> getFlickrTrending throws ClientProtocolException, IOException, JSONException {
+  	HttpClient client = new DefaultHttpClient();
+  	HttpGet request = new HttpGet("https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key="+key+"&format=json&nojsoncallback=1");
+  	HttpResponse response = client.execute(request);
+  	BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
+  	StringBuilder result = new StringBuilder();
+  	String temp = "";  	
+  	while ((temp = rd.readLine()) != null) {      
+	    result.append(temp);    
+  	}
+
+  	JSONObject obj = new JSONObject(result.toString());  
+    JSONArray photos = obj.getJSONObject("photos").getJSONArray("photo");  
+
+    List<FlickrObject> flickrObjects = new ArrayList<FlickrObject>();
+
+    for (int i = 0; i < photos.length(); i++) {
+      JSONObject o = photos.getJSONObject(i);
+
+      String imageId = o.getString("id");
+      String secret = o.getString("secret");
+      String server = o.getString("server");
+      int farm = o.getInt("farm");
+      String title = o.getString("title");
+
+      FlickrObject fo = new FlickrObject(imageId,secret,server,farm,title);
+      flickrObjects.add(fo);
+    }
+    System.out.println(flickrObjects.toString());
+    return flickrObjects;
+ } 
+}
