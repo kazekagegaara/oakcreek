7d01056ed43d82147cf079de49d49839d0e287a7
mohitkk <mkumar27@asu.edu>
2015-11-28 19:39:47 -0700

    testing. not important

--- backend/Services/src/main/java/SentimentGenerator/HttpClientHelper.java
+++ backend/Services/src/main/java/SentimentGenerator/HttpClientHelper.java
@@ -5,10 +5,11 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import java.io.IOException;
 
 public class HttpClientHelper {
 
-	public HttpResponse serviceCall(String url) throws ClientProtocolException{
+	public HttpResponse serviceCall(String url) throws ClientProtocolException, IOException{
 		HttpClient client = new DefaultHttpClient();
 
 		HttpGet request = new HttpGet(url);
