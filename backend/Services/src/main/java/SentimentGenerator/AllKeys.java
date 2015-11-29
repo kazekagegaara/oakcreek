10c4ce43e714fb569a6726d3110f46dad3d9bd7f
kazekagegaara <mansinkal2910@yahoo.co.in>
2015-11-28 19:21:17 -0700

    adding extra keys for twitter and twitter4j jar

--- backend/Services/src/main/java/SentimentGenerator/AllKeys.java
+++ backend/Services/src/main/java/SentimentGenerator/AllKeys.java
@@ -1,13 +1,25 @@
 package SentimentGenerator;
 
 public class AllKeys{
-	private static final String twitterKey = "";
+	private static final String twitterConsumerKey = "";
+	private static final String twitterConsumerSecret = "";
+	private static final String twitterAccessToken = "";
+	private static final String twitterAccessTokenSecret = "";
 	private static final String youtubeKey = "";
 	private static final String flickerKey = "";
 	private static final String alchemyKey = "";
 
-	public String getTwitterKey(){
-		return this.twitterKey;
+	public String getTwitterConsumerKey(){
+		return this.twitterConsumerKey;
+	}
+	public String getTwitterConsumerSecret(){
+		return this.twitterConsumerSecret;
+	}
+	public String getTwitterAccessToken(){
+		return this.twitterAccessToken;
+	}
+	public String getTwitterAccessTokenSecret(){
+		return this.twitterAccessTokenSecret;
 	}
 	public String getYoutubeKey(){
 		return this.youtubeKey;
